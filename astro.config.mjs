6b5a4b30d3060d9188383ee0946de838ec5c6deb
Barbod Meshki <barbod@example.com>
2026-06-09 15:14:52 +0330

    ...

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,9 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
-  site: "https://barbodclassicalmusic.com",
+  site: "https://barbodclassicalmusic.github.io",
+  output: "static",
   integrations: [react(), mdx(), sitemap()],
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true,
-    },
-  }),
 });
